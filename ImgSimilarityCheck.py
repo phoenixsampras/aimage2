8ee201a10fcebf79cf52204ddf92abb12c58956b
maitreyamaity <maitreya.maity@gmail.com>
2018-10-21 02:37:26 +0530

    Update ImgSimilarityCheck.py

--- ImgSimilarityCheck.py
+++ ImgSimilarityCheck.py
@@ -1,5 +1,5 @@
 # install these dependencies
-# sudo pip3 install opencv-contrib-python image_match elasticsearch scipy
+# sudo pip3 install opencv-contrib-python image_match elasticsearch scipy skimage
 
 
 import os
@@ -8,6 +8,7 @@ import cv2
 import numpy as np
 import pickle
 from pathlib import Path
+from skimage import morphology
 from image_match.goldberg import ImageSignature
 
 # Flask stuff
@@ -18,7 +19,8 @@ from datetime import datetime
 app = Flask(__name__)
 FLASK_DEBUG = 1
 
-def createDB(imgDirectoryPath,dbFilePath):
+
+def createDB(imgDirectoryPath, dbFilePath):
     distances = []
     path = imgDirectoryPath
     rx = re.compile(r'\.(bmp)')
@@ -26,16 +28,26 @@ def createDB(imgDirectoryPath,dbFilePath):
         for x in fnames:
             if rx.search(x):
                 imgpath = os.path.join(path, x)
-                print('Creating index...',imgpath)
+                print('Creating index...', imgpath)
                 imgname = os.path.splitext(os.path.basename(imgpath))[0]
                 img = cv2.imread(imgpath)
                 dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
-                median = cv2.medianBlur(dst, 7)
-                gray = cv2.cvtColor(median, cv2.COLOR_BGR2GRAY)
+                gaussian_3 = cv2.GaussianBlur(dst, (9, 9), 10.0)
+                sharpened = cv2.addWeighted(dst, 1.5, gaussian_3, -0.5, 0, dst)
+                gray = cv2.cvtColor(sharpened, cv2.COLOR_BGR2GRAY)
                 th, im_thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-                kernel = np.ones((7, 7), dtype=np.uint8)
+                kernel = np.ones((3, 3), dtype=np.uint8)
                 binary_cleaned = cv2.morphologyEx(im_thresh, cv2.MORPH_OPEN, kernel)
-                localimg = np.array(binary_cleaned)
+                binary_mask = cv2.bitwise_not(binary_cleaned)
+                imglab = morphology.label(binary_mask)
+                cleaned = morphology.remove_small_objects(imglab, min_size=500, connectivity=8)
+                img3 = np.zeros((cleaned.shape))
+                img3[cleaned > 0] = 255
+                img3 = np.uint8(img3)
+                clean_image = cv2.bitwise_not(img3)
+                clean_imagecol = cv2.cvtColor(clean_image, cv2.COLOR_GRAY2RGB)
+                localimg = np.array(clean_imagecol)
+                # ar = {'name': imgname + '.bmp', 'path': 'static/6332/'+imgname + '.bmp', 'data': localimg}
                 ar = {'name': imgname + '.bmp', 'path': imgpath, 'data': localimg}
                 distances.append(ar)
 
@@ -43,8 +55,8 @@ def createDB(imgDirectoryPath,dbFilePath):
         # store the data as binary data stream
         pickle.dump(distances, filehandle)
 
-def doSearch(dbFilePath,imgDirectoryPath, testImagePath,topSearch ):
 
+def doSearch(dbFilePath, imgDirectoryPath, testImagePath, topSearch):
     # dbFilePath : Database data file path
     # imgDirectoryPath : Directory path where all images stored
     # testImagePath : Image path of test sample
@@ -57,21 +69,31 @@ def doSearch(dbFilePath,imgDirectoryPath, testImagePath,topSearch ):
     else:
         print('Not Found DB File... Creating new One')
         createDB(imgDirectoryPath, dbFilePath)
-        fixeddistances = searchSimilarImages(dbFilePath,testImagePath,topSearch)
+        fixeddistances = searchSimilarImages(dbFilePath, testImagePath, topSearch)
 
     return fixeddistances
 
-def searchSimilarImages(dbFilePath, testImagePath,topSearch):
+
+def searchSimilarImages(dbFilePath, testImagePath, topSearch):
     print('Starting Search')
     distancemap = []
     img = cv2.imread(testImagePath)
     dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
-    median = cv2.medianBlur(dst, 7)
-    gray = cv2.cvtColor(median, cv2.COLOR_BGR2GRAY)
+    gaussian_3 = cv2.GaussianBlur(dst, (9, 9), 10.0)
+    sharpened = cv2.addWeighted(dst, 1.5, gaussian_3, -0.5, 0, dst)
+    gray = cv2.cvtColor(sharpened, cv2.COLOR_BGR2GRAY)
     th, im_thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    kernel = np.ones((7, 7), dtype=np.uint8)
+    kernel = np.ones((3, 3), dtype=np.uint8)
     binary_cleaned = cv2.morphologyEx(im_thresh, cv2.MORPH_OPEN, kernel)
-    testdata = np.array(binary_cleaned)
+    binary_mask = cv2.bitwise_not(binary_cleaned)
+    imglab = morphology.label(binary_mask)
+    cleaned = morphology.remove_small_objects(imglab, min_size=500, connectivity=8)
+    img3 = np.zeros((cleaned.shape))
+    img3[cleaned > 0] = 255
+    img3 = np.uint8(img3)
+    clean_image = cv2.bitwise_not(img3)
+    clean_imagecol = cv2.cvtColor(clean_image, cv2.COLOR_GRAY2RGB)
+    testdata = np.array(clean_imagecol)
 
     with open(dbFilePath, 'rb') as filehandle:
         # read the data as binary data stream
@@ -104,12 +126,12 @@ def index():
         img = Image.open(file.stream)  # PIL image
         uploaded_img_path = "static/upload/" + datetime.now().isoformat() + "_" + file.filename
         img.save(uploaded_img_path)
-        scores = doSearch('static/localdb.data','static/6332', uploaded_img_path, 10)
+        scores = doSearch('static/localdb.data', 'static/6332', uploaded_img_path, 100)
         print(scores)
-        return render_template('index.html',query_path=uploaded_img_path,scores=scores)
+        return render_template('index.html', query_path=uploaded_img_path, scores=scores)
     else:
         return render_template('index.html')
 
 
 if __name__ == "__main__":
-	app.run("0.0.0.0")
+    app.run("0.0.0.0")
