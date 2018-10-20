b1782a1af530cdc28236684439f63ac0ebb5be7b
maitreyamaity <maitreya.maity@gmail.com>
2018-10-20 21:30:11 +0530

    Update ImgSimilarityCheck.py

--- ImgSimilarityCheck.py
+++ ImgSimilarityCheck.py
@@ -5,6 +5,10 @@
 import os
 import re
 import cv2
+import numpy as np
+import pickle
+from pathlib import Path
+from image_match.goldberg import ImageSignature
 
 # Flask stuff
 from PIL import Image
@@ -14,76 +18,97 @@ from datetime import datetime
 app = Flask(__name__)
 FLASK_DEBUG = 1
 
-from image_match.goldberg import ImageSignature
+def createDB(imgDirectoryPath,dbFilePath):
+    distances = []
+    path = imgDirectoryPath
+    rx = re.compile(r'\.(bmp)')
+    for path, dnames, fnames in os.walk(path):
+        for x in fnames:
+            if rx.search(x):
+                imgpath = os.path.join(path, x)
+                print('Creating index...',imgpath)
+                imgname = os.path.splitext(os.path.basename(imgpath))[0]
+                img = cv2.imread(imgpath)
+                dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
+                median = cv2.medianBlur(dst, 7)
+                gray = cv2.cvtColor(median, cv2.COLOR_BGR2GRAY)
+                th, im_thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                kernel = np.ones((7, 7), dtype=np.uint8)
+                binary_cleaned = cv2.morphologyEx(im_thresh, cv2.MORPH_OPEN, kernel)
+                localimg = np.array(binary_cleaned)
+                ar = {'name': imgname + '.bmp', 'path': imgpath, 'data': localimg}
+                distances.append(ar)
 
+    with open(dbFilePath, 'wb') as filehandle:
+        # store the data as binary data stream
+        pickle.dump(distances, filehandle)
 
-# def main():
-#     searchSimilarImage('/mnt/c/Users/Phoenix Sampras/Documents/bb/fegasacruz/6332','/mnt/c/Users/Phoenix Sampras/Documents/bb/fegasacruz/test/2.bmp',10)
+def doSearch(dbFilePath,imgDirectoryPath, testImagePath,topSearch ):
 
-def searchSimilarImage(imgDirectoryPath, testImagePath, topSearch):
-	# imgDirectoryPath : Directory path where all images stored
-	# testImagePath : Image path of test sample
-	# topSearch : Numeric value to show top # images
+    # dbFilePath : Database data file path
+    # imgDirectoryPath : Directory path where all images stored
+    # testImagePath : Image path of test sample
+    # topSearch : Numeric value to show top # images
+    fixeddistances = []
+    db_file = Path(dbFilePath)
+    if db_file.is_file():
+        print('Found DB File')
+        fixeddistances = searchSimilarImages(dbFilePath, testImagePath, topSearch)
+    else:
+        print('Not Found DB File... Creating new One')
+        createDB(imgDirectoryPath, dbFilePath)
+        fixeddistances = searchSimilarImages(dbFilePath,testImagePath,topSearch)
 
-	fixeddistances = []
-	distances = []
-	path = imgDirectoryPath
-	rx = re.compile(r'\.(bmp)')
-	for path, dnames, fnames in os.walk(path):
-		for x in fnames:
-			if rx.search(x):
-				imgpath = os.path.join(path, x)
-				imgname = os.path.splitext(os.path.basename(imgpath))[0]
-				#print(imgname)
-				gis = ImageSignature()
-				a = gis.generate_signature(testImagePath)
-				b = gis.generate_signature(imgpath)
-				dis = (gis.normalized_distance(a, b))
-				ar = {'name': imgname + '.bmp', 'path': imgpath, 'distance': dis}
-				distances.append(ar)
-	newlist =  sorted(distances, key=lambda k: k['distance'])
-	for i in range(topSearch):
-		fixeddistances.append(newlist[i])
-	return fixeddistances
+    return fixeddistances
 
+def searchSimilarImages(dbFilePath, testImagePath,topSearch):
+    print('Starting Search')
+    distancemap = []
+    img = cv2.imread(testImagePath)
+    dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
+    median = cv2.medianBlur(dst, 7)
+    gray = cv2.cvtColor(median, cv2.COLOR_BGR2GRAY)
+    th, im_thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    kernel = np.ones((7, 7), dtype=np.uint8)
+    binary_cleaned = cv2.morphologyEx(im_thresh, cv2.MORPH_OPEN, kernel)
+    testdata = np.array(binary_cleaned)
 
+    with open(dbFilePath, 'rb') as filehandle:
+        # read the data as binary data stream
+        distances = pickle.load(filehandle)
+        for i in range(len(distances)):
+            imgname = distances[i].get('name')
+            imgpath = distances[i].get('path')
+            localdata = distances[i].get('data')
+            gis = ImageSignature()
+            a = gis.generate_signature(testdata)
+            b = gis.generate_signature(localdata)
+            dis = (gis.normalized_distance(a, b))
+            ar = {'name': imgname + '.bmp', 'path': imgpath, 'distance': dis}
+            distancemap.append(ar)
 
-# return [(dists[id], img_paths[id]) for id in ids]
+    newlist = sorted(distancemap, key=lambda k: k['distance'])
+    fixeddistances = []
+    for i in range(topSearch):
+        fixeddistances.append(newlist[i])
+    return fixeddistances
 
-
-# for i in range(topSearch):
-#     img = cv2.imread(newlist[i].get('path'))
-#     cv2.imshow(newlist[i].get('name'),img)
-#     print(newlist[i].get('path'))
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
 # if __name__ == '__main__':
 #     main()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == 'POST':
-		file = request.files['query_img']
-
-		img = Image.open(file.stream)  # PIL image
-		uploaded_img_path = "static/upload/" + datetime.now().isoformat() + "_" + file.filename
-		img.save(uploaded_img_path)
-
-		scores = searchSimilarImage('static/6332', uploaded_img_path, 100)
-
-		print(scores)
-
-		# query = fe.extract(img)
-		# dists = np.linalg.norm(features - query, axis=1)  # Do search
-		# ids = np.argsort(dists)[:300] # Top 300 results
-		# scores = [(dists[id], img_paths[id]) for id in ids]
-
-		return render_template('index.html',
-							   query_path=uploaded_img_path,
-							   scores=scores)
-	else:
-		return render_template('index.html')
+    if request.method == 'POST':
+        file = request.files['query_img']
+        img = Image.open(file.stream)  # PIL image
+        uploaded_img_path = "static/upload/" + datetime.now().isoformat() + "_" + file.filename
+        img.save(uploaded_img_path)
+        scores = doSearch('static/localdb.data','static/6332', uploaded_img_path, 10)
+        print(scores)
+        return render_template('index.html',query_path=uploaded_img_path,scores=scores)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
